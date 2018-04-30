e52559b54b29af1228b3f5f4426d7dc4b298bbbe
Paul Fryer <paul@fryer.org.uk>
2018-04-30 21:57:17 +0100

    Update PF_InitScript.sh

--- PF_InitScript.sh
+++ PF_InitScript.sh
@@ -21,7 +21,7 @@ sudo yum update -y
 sudo yum install -y libselinux libselinux-utils libselinux-utils selinux-policy-minimum selinux-policy-mls selinux-policy-targeted policycoreutils
 
 # Install the web tools and required packages
-sudo yum install -y yum-cron nginx php72u-fpm php72u-pecl-yaml
+sudo yum install -y yum-cron nginx php72u-fpm php72u-pdo
 
 # Enable the services
 sudo systemctl enable nginx.service
@@ -43,16 +43,8 @@ cd ~
 # Config the webserver
 
 sudo rm /etc/nginx/default.d/php.conf
-sudo echo "# Pass PHP scripts to PHP-FPM
-location ~* \.php$ {
-    fastcgi_index   index.php;
-    fastcgi_pass    127.0.0.1:9000;
-    #fastcgi_pass   unix:/var/run/php-fpm/php-fpm.sock;
-    include         fastcgi_params;
-    fastcgi_param   SCRIPT_FILENAME    $document_root$fastcgi_script_name;
-    fastcgi_param   SCRIPT_NAME        $fastcgi_script_name;
-}" > /etc/nginx/default.d/php.conf
-
+wget https://raw.githubusercontent.com/fregster/Scripts/master/php.conf
+sudo mv php.conf /etc/nginx/default.d/
 
 
 # Start the services
